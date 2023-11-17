80edc539d47aa0c05ef8c16b921e658f68a328f5
Zoe Fisk <jedijewel04@gmail.com>
2023-11-17 14:38:04 -0500

    got the player moving!!!

--- IMGD Project/Assets/playerMover.cs
+++ IMGD Project/Assets/playerMover.cs
@@ -1,17 +1,17 @@
 using Godot;
 using System;
 
-public partial class playerMover : Node
+public partial class playerMover : CharacterBody2D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+	[Export] public float speed = 150f;
+
+	public override void _PhysicsProcess(double delta)
 	{
+		//LookAt(GetGlobalMousePosition());
 
-	}
+		Vector2 move_input = Input.GetVector("move_left", "move_right", "move_up", "move_down");
+		Velocity = move_input * speed;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-
+		MoveAndSlide();
 	}
 }
