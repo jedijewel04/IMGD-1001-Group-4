4ef81c32dbcb57f232d0c79a821ecdcf33bd2d1d
jedijewel04 <jedijewel04@gmail.com>
2023-12-12 17:55:47 -0500

    the game is working now!!!

--- IMGD Project/Assets/Scripts/Global.cs
+++ IMGD Project/Assets/Scripts/Global.cs
@@ -7,10 +7,29 @@ public partial class Global : Node
 	
 	public String currentTask;
 
+	public TextureRect sendEmailCrossed;
+	public TextureRect drinkCoffeeCrossed;
+	public TextureRect meetingCrossed;
+	public TextureRect useToiletCrossed;
+	public TextureRect eatSnackCrossed;
+	public TextureRect printCrossed;
+	public TextureRect introduceCrossed;
+
+	public int tasksCompleted = 0;
+	public bool talkedToVeronica = false;
+
 	public static Global Instance;
 
 	public override void _Ready()
 	{
 		Instance = this;
+
+		sendEmailCrossed = GetNode<TextureRect>("/root/HUD/Expanded_Tasks/send_email_crossed");
+		drinkCoffeeCrossed = GetNode<TextureRect>("/root/HUD/Expanded_Tasks/drink_coffee_crossed");
+		meetingCrossed = GetNode<TextureRect>("/root/HUD/Expanded_Tasks/attend_meeting_crossed");
+		useToiletCrossed = GetNode<TextureRect>("/root/HUD/Expanded_Tasks/use_toilet_crossed");
+		eatSnackCrossed = GetNode<TextureRect>("/root/HUD/Expanded_Tasks/get_snack_crossed");
+		printCrossed = GetNode<TextureRect>("/root/HUD/Expanded_Tasks/print_documents_crossed");
+		introduceCrossed = GetNode<TextureRect>("/root/HUD/Expanded_Tasks/introduce_crossed");
 	}
 }
--- IMGD Project/Assets/Scripts/SuperScene.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	var dialog = Dialogic.start("convo_1")
-	add_child(dialog)
-
